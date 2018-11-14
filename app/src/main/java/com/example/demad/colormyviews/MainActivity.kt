9465e95681711b57166d9cc1bdf8a2d671bcab3c
Innocent Angwadia <demsdema217@gmail.com>
2018-11-14 22:06:36 +0000

    add buttons and colors

--- app/src/main/java/com/example/demad/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/demad/colormyviews/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.demad.colormyviews
 
 import android.graphics.Color
+import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,7 +17,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListener() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_text_two, box_text_three, box_text_four, box_text_five, constraint)
+            listOf(
+                box_one_text,
+                box_text_two,
+                box_text_three,
+                box_text_four,
+                box_text_five,
+                constraint,
+                red_button,
+                yellow_button,
+                green_button
+            )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
@@ -33,6 +44,13 @@ class MainActivity : AppCompatActivity() {
             R.id.box_text_three -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_text_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_text_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+//            R.id.red_button -> box_text_three.setBackgroundResource(R.color.my_red)
+//            R.id.red_button -> view.setBackgroundResource(R.color.my_red)
+            R.id.red_button -> constraint.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_text_four.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_text_five.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
