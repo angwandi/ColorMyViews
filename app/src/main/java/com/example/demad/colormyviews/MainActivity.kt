a8321e3a94141b74db863d9ba8d00e817e9620ed
Innocent Angwadia <demsdema217@gmail.com>
2018-11-14 21:15:21 +0000

    add handler for colors

--- app/src/main/java/com/example/demad/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/demad/colormyviews/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.demad.colormyviews
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListener()
+    }
+
+    private fun setListener() {
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_text_two, box_text_three, box_text_four, box_text_five, constraint)
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_text_two -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_text_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_text_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_text_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
